96fc59d55659169c113dc00fbfa79802732e2155
HA1 <hai13032001@gmail.com>
2023-12-25 09:50:48 +0700

    Initialization

---
+++ setup.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+exit_status() {
+  if [ $? -eq 0 ]; then
+    echo
+    echo "***   Command Execution Was Successful.   ***"
+    echo
+    source ~/.bashrc
+  else
+    echo
+    echo "-------------------------------------------------------"
+    echo "     [Error] Process Command Execution Has Failed!     "
+    echo "-------------------------------------------------------"
+    echo
+
+    read -p "The last command has exited with an error. Exit script? (Yes/no)" answer
+    if [ "$answer" == "yes" || "$answer" == "y" || -z "$answer" ]; then
+      exit 1
+    fi
+  fi
+}
+
+msg() {
+  message=$1
+  echo
+  echo "--------------------------------------------"
+  echo "***>>> $message <<<***"
+  echo "--------------------------------------------"
+  echo
+}
+
+exitUpdate() {
+  echo
+  echo "-------------------------------------------------------"
+  echo ">>>>   Operating System Update Has Been Completed  <<<<"
+  echo "-------------------------------------------------------"
+  echo
+  clear
+  source ~/.bashrc
+  exec bash
+}
+
+#UpdateOS
+updateOS() {
+  msg "Initialization Update Operating System"
+  #
+  sudo apt update
+  exit_status
+
+  sudo apt upgrade -y
+  exit_status
+
+  sudo apt install git
+  exit_status
+
+  sudo apt install curl
+  exit_status
+}
+
+#Install NodeJS
+installNode() {
+  msg "Start Install NodeJS"
+  #
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+  exit_status
+
+  export NVM_DIR="$HOME/.nvm"
+  source "$NVM_DIR/nvm.sh"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+  if command -v nvm &>/dev/null; then
+    nvm install v18.17.1
+  else
+    echo "Failed to load nvm"
+  fi
+}
+
+#Install Docker
+installDocker() {
+  msg "Start Install Docker"
+  #
+  sudo apt update
+
+  sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+  exit_status
+
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+
+  sudo apt update
+  apt-cache policy docker-ce
+
+  sudo apt install docker-ce -y
+  exit_status
+
+  sudo systemctl status docker --no-pager
+  exit_status
+
+  mkdir -p ~/.docker/cli-plugins/
+  curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+  exit_status
+
+  chmod +x ~/.docker/cli-plugins/docker-compose
+  sudo usermod -aG docker ${USER}
+  exit_status
+}
+
+##################################
+updateOS
+installNode
+installDocker
+exitUpdate
